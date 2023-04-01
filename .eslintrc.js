2c51c67c9aab2e79278587b55353304897b001d8
Ersin <respawm19@gmail.com>
2023-04-01 23:15:52 +0300

    added starter homepage

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    plugins: ["@html-eslint"],
+    overrides: [
+        {
+           files: ["*.html"],
+           parser: "@html-eslint/parser",
+           extends: ["plugin:@html-eslint/recommended"],
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+    }
+}
